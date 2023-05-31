8731658f765804c68bf3a5ba8de479bf3e3966c2
Guillaume Teillais <guillaume.teillais@gmail.com>
2023-05-31 19:01:31 +0200

    add params on controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,4 +7,30 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  private
+
+  def car_params
+    params.require(:car).permit(
+      :marque,
+      :modele,
+      :version,
+      :annee,
+      :prix,
+      :date_mise_en_circulation,
+      :genre,
+      :energie,
+      :kilometrage,
+      :puissance_chevaux,
+      :puissance_fiscale,
+      :nb_places,
+      :nb_portes,
+      :couleur,
+      :boite_de_vitesse,
+      :nb_rapports,
+      :garantie,
+      :categorie,
+      photos: []
+    )
+  end
 end
